9e073fd8f5c6f87a6d0ca3c9237a38674a9d4e32
krzysztof <krzys2690@wp.pl>
2024-03-05 18:59:50 +0100

    Update

--- Background_MainWindow.py
+++ Background_MainWindow.py
@@ -36,15 +36,28 @@ def fn_ConenctToDatabase():
         print(f'Blad polaczenia {e}')
 
 
-
+#Screen DialogClothes
 def fn_GetListClothes(v_type, v_size,):
-    global cursor
     cursor = connection.cursor()
-    query = f"SELECT  t_clothes.c_id,t_clothes.c_type, t_clothes.c_size, t_employee.c_firstname,t_employee.c_lastname FROM t_clothes LEFT JOIN t_employee ON t_clothes.c_id_employee = t_employee.c_id WHERE c_size LIKE '{v_size}%' AND c_type LIKE '{v_type}%';"
+    query = (f"SELECT  t_clothes.c_id,t_clothes.c_type, t_clothes.c_size, t_employee.c_firstname,t_employee.c_lastname FROM t_clothes LEFT JOIN t_employee ON t_clothes.c_id_employee = t_employee.c_id WHERE c_size "
+             f"LIKE '{v_size}%' AND c_type LIKE '{v_type}%';")
     cursor.execute(query)
     results = cursor.fetchall()
     return results
     cursor.close()
 
+#Screen DialogSearchPerson
 
-
+def fn_GetListEmployee(v_firstname, v_lastname):
+    cursor = connection.cursor()
+    print(v_firstname, v_lastname)
+    query = f"SELECT * FROM t_employee WHERE c_firstname LIKE '{v_firstname}%' AND c_lastname LIKE '{v_lastname}%';"
+    cursor.execute(query)
+    results = cursor.fetchall()
+    return results
+def fn_AssignCloth(v_idcloth,v_idperson):
+    cursor = connection.cursor()
+    query = f"UPDATE t_clothes SET c_id_employee = {v_idperson} WHERE c_id = {v_idcloth};"
+    cursor.execute(query)
+    connection.commit()
+    cursor.close()
--- DialogSearchPerson.py
+++ DialogSearchPerson.py
@@ -3,6 +3,25 @@ import Background_MainWindow
 
 class Ui_Dialog(object):
 
+    def fn_AssignClothToEmployee(self):
+        from DialogClothes import v_idcloth
+        v_idemployee = self.fn_GetIDEmployee()
+        print(v_idcloth,v_idemployee)
+        Background_MainWindow.fn_AssignCloth(v_idcloth,v_idemployee)
+    def fn_EmployeeToList(self):
+        self.listWidget.clear()
+        v_firstname = self.lineEdit_FirstName.text()
+        v_lastname = self.lineEdit_LastName.text()
+        results = Background_MainWindow.fn_GetListEmployee(v_firstname,v_lastname)
+        for employee in results:
+            text = f'{employee[0]}: {employee[1]} {employee[2]}'
+            self.listWidget.addItem(text)
+    def fn_GetIDEmployee(self):
+        text = self.listWidget.currentItem().text()
+        text = text.split(':')
+        v_idemployee = text[0]
+        return v_idemployee
+
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(515, 322)
@@ -18,22 +37,21 @@ class Ui_Dialog(object):
         self.label_FirstName = QtWidgets.QLabel(self.horizontalLayoutWidget)
         self.label_FirstName.setObjectName("label_FirstName")
         self.horizontalLayout.addWidget(self.label_FirstName)
-        self.lineEdi_FirstName = QtWidgets.QLineEdit(self.horizontalLayoutWidget)
-        self.lineEdi_FirstName.setObjectName("lineEdi_FirstName")
-        self.horizontalLayout.addWidget(self.lineEdi_FirstName)
+        self.lineEdit_FirstName = QtWidgets.QLineEdit(self.horizontalLayoutWidget)
+        self.lineEdit_FirstName.setObjectName("lineEdit_FirstName")
+        self.horizontalLayout.addWidget(self.lineEdit_FirstName)
         self.label_LastName = QtWidgets.QLabel(self.horizontalLayoutWidget)
         self.label_LastName.setObjectName("label_LastName")
         self.horizontalLayout.addWidget(self.label_LastName)
         self.lineEdit_LastName = QtWidgets.QLineEdit(self.horizontalLayoutWidget)
         self.lineEdit_LastName.setObjectName("lineEdit_LastName")
         self.horizontalLayout.addWidget(self.lineEdit_LastName)
-        self.pushButton_Search = QtWidgets.QPushButton(self.horizontalLayoutWidget)
+        self.pushButton_Search = QtWidgets.QPushButton(self.horizontalLayoutWidget, clicked = lambda: self.fn_EmployeeToList())
         self.pushButton_Search.setObjectName("pushButton_Search")
         self.horizontalLayout.addWidget(self.pushButton_Search)
-        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton = QtWidgets.QPushButton(Dialog, clicked = lambda: self.fn_AssignClothToEmployee())
         self.pushButton.setGeometry(QtCore.QRect(390, 280, 111, 28))
         self.pushButton.setObjectName("pushButton")
-
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
@@ -46,6 +64,7 @@ class Ui_Dialog(object):
         self.pushButton.setText(_translate("Dialog", "Potwierdz"))
 
 
+
 if __name__ == '__main__':
     import sys
     app = QtWidgets.QApplication(sys.argv)
@@ -53,4 +72,5 @@ if __name__ == '__main__':
     ui = Ui_Dialog()
     ui.setupUi(window_SearchPerson)
     window_SearchPerson.show()
+
     sys.exit(app.exec_())
